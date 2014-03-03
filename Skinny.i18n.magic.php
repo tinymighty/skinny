540dab12a83e6b130b010d937a2730382d9331ac
Andru Vallance <andru@tinymighty.com>
2014-03-03 17:12:08 +0100

    Magic word for imageurl parser function

--- Skinny.i18n.magic.php
+++ Skinny.i18n.magic.php
@@ -15,5 +15,6 @@ $magicWords['en'] = array(
 	'setskin' => array( 0, 'setskin', 'set skin' ),
 	'layout' => array(0, 'layout', 'set layout', 'setlayout'),
 	'movetoskin' => array( 0, 'movetoskin', 'move to skin' ),
-	'skinsert' => array(0, 'skinsert', 'skin insert')
+	'skinsert' => array(0, 'skinsert', 'skin insert'),
+	'imageurl' => array(0, 'imageurl', 'image url')
 );
